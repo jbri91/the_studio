b8001a5e4555c2d61c5cc6b5704930f2d67919dc
Johnny Briones <jbrrii91@gmail.com>
2021-11-07 22:16:06 -0500

    Added Buttons to sign in and sign out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navigation from './Components/Navigation'
 
+
 function App() {
   return (
     <div className="App">
-  
         <Navigation />
+        
      
     </div>
   );
